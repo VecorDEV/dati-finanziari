27cc24609cea2f503e9198a79480d240fa25a1f6
VecorDEV <vector.devv@gmail.com>
2025-02-14 16:50:28 +0100

    Update predict_stock.py

--- scripts/predict_stock.py
+++ scripts/predict_stock.py
@@ -23,7 +23,7 @@ def get_stock_news(symbol):
     feed = feedparser.parse(url)
     
     titles = [entry.title for entry in feed.entries]
-        print(titles)
+    print(titles)
     return titles
 
 # Dizionario di parole chiave con il loro punteggio di sentiment
