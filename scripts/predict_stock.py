f962a392959ed5f6f560479575ff127a98b21e67
vectordev <vector.devv@gmail.com>
2025-02-15 15:52:07 +0100

    Update predict_stock.py

--- scripts/predict_stock.py
+++ scripts/predict_stock.py
@@ -28,7 +28,7 @@ symbol_list = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "V", "JPM", "JNJ
         "AUDJPY", "CADJPY", "CHFJPY", "EURAUD", "EURNZD", "EURCAD", "EURCHF", "GBPCHF", "GBPJPY", "AUDCAD",
         "BTCUSD", "ETHUSD", "LTCUSD", "XRPUSD", "BCHUSD", "EOSUSD", "XLMUSD", "ADAUSD", "TRXUSD", "NEOUSD",
         "DASHUSD", "XMRUSD", "ETCUSD", "ZECUSD", "BNBUSD", "DOGEUSD", "USDTUSD", "LINKUSD", "ATOMUSD", "XTZUSD",
-        "GOLD", "COCOA", "OIL"]  # Puoi aggiungere altri simboli
+        "COCOA", "XAUUSD", "XAGUSD", "OIL"]  # Puoi aggiungere altri simboli
 
 def get_stock_news(symbol):
     """ Recupera i titoli delle notizie per un determinato simbolo. """
