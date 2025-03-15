3ba7a9c32f72452da3e754baa994cac174b93374
vectordev <vector.devv@gmail.com>
2025-03-15 16:18:25 +0100

    Update predict_PRO.py

--- scripts/predict_PRO.py
+++ scripts/predict_PRO.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Configura le opzioni per il browser headless
@@ -21,14 +23,18 @@ driver.get(url)
 # Aspetta che la pagina venga caricata completamente (modifica il tempo se necessario)
 time.sleep(5)  # Puoi aumentare questo tempo se il caricamento della pagina è lento
 
-# Trova l'elemento che contiene la percentuale del sentiment usando la classe identificata
-sentiment_element = driver.find_element(By.CLASS_NAME, 'styles-module__range_value___089bM')
-
-# Ottieni il testo del sentiment
-sentiment_text = sentiment_element.text
-
-# Stampa il risultato
-print("Sentiment trovato:", sentiment_text)
+try:
+    # Aspetta fino a quando l'elemento sentiment è visibile (modifica la classe se necessario)
+    sentiment_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'styles-module__range_value___089bM'))
+    )
+    
+    # Ottieni il testo del sentiment
+    sentiment_text = sentiment_element.text
+    print("Sentiment trovato:", sentiment_text)
+    
+except Exception as e:
+    print("Errore durante la ricerca del sentiment:", e)
 
 # Chiudi il driver
 driver.quit()
